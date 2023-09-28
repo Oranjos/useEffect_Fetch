22deb009cfcc052af2027a1a9e2035b15fbe625a
david Karanja <davidkaranja@davids-MacBook-Pro.local>
2023-09-28 12:32:53 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MultipleReturnsFetchData from "./assets/Returns";
+
+function App() {
+  return (
+    <div>
+      <MultipleReturnsFetchData />
+    </div>
+  );
+}
+
+export default App;
